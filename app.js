a491acc651fbe6a817802a17bcbac68697eb4bca
Ghevond Tunyan <ghtunyan@gmail.com>
2022-06-30 19:16:11 +0200

    fetch products and diplay them

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log('products starter');
+const url = 'https://course-api.com/javascript-store-products/';
+
+const productDOM = document.querySelector('.products-center');
+
+
+const fetchProduct = async () => {
+    productDOM.innerHTML = `<div class="loading"></div>`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data
+    } catch (error) {
+        productDOM.innerHTML = `<p class="error"> there was an error</p>`
+    }
+};
+
+const displayProducts = (list) => {
+    const productsList = list.map(product => {
+        const { id } = product;
+        const { name: title, price } = product.fields;
+        const { url: img } = product.fields.image[0];
+        const formatPrice = price / 100
+        return `
+    <a href="product.html?id=${id}&name=john
+    " class="single-product">
+          <img src="${img}" alt="lorem" class="single-product-img img">
+          <footer>
+            <h5 class="name">${title}</h5>
+            <span class="price">$${formatPrice}</span>
+          </footer>
+        </a>
+    `
+    }).join('');
+    productDOM.innerHTML = `<div class="products-container">${productsList}</div>`
+
+}
+
+const start = async () => {
+    const data = await fetchProduct();
+    displayProducts(data)
+}
+
+start()
